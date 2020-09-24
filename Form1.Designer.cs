43b303abcd66b50b1ff998a669fcd1c02c12a986
GabrielRioo <Gabrielpaivario@gmail.com>
2020-09-24 15:05:53 -0300

    create the project and the interface

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace EstudoWinForm
+{
+	partial class Inicio
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.salvarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.txbJanela = new System.Windows.Forms.TextBox();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.editarToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// arquivoToolStripMenuItem
+			// 
+			this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.salvarToolStripMenuItem,
+            this.sairToolStripMenuItem});
+			this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+			this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+			this.arquivoToolStripMenuItem.Text = "Arquivo";
+			// 
+			// editarToolStripMenuItem
+			// 
+			this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+			this.editarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+			this.editarToolStripMenuItem.Text = "Editar";
+			// 
+			// abrirToolStripMenuItem
+			// 
+			this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+			this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.abrirToolStripMenuItem.Text = "Abrir";
+			// 
+			// salvarToolStripMenuItem
+			// 
+			this.salvarToolStripMenuItem.Name = "salvarToolStripMenuItem";
+			this.salvarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.salvarToolStripMenuItem.Text = "Salvar";
+			// 
+			// sairToolStripMenuItem
+			// 
+			this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+			this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.sairToolStripMenuItem.Text = "Sair";
+			// 
+			// txbJanela
+			// 
+			this.txbJanela.Location = new System.Drawing.Point(12, 27);
+			this.txbJanela.Multiline = true;
+			this.txbJanela.Name = "txbJanela";
+			this.txbJanela.Size = new System.Drawing.Size(776, 411);
+			this.txbJanela.TabIndex = 1;
+			// 
+			// Inicio
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.txbJanela);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Inicio";
+			this.Text = "Bloco de Notas";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem salvarToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+		private System.Windows.Forms.TextBox txbJanela;
+	}
+}
+
